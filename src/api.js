8676b62d1e6721029fe7142aa5e76c2c2e3f7cb4
rom2 guer <romainguerin033@gmail.com>
2025-10-16 19:25:47 +0200

    Ajout du workflow CI/CD

--- src/api.js
+++ src/api.js
@@ -3,6 +3,8 @@ const fetch = require('node-fetch');
 async function getCitation() {
   const response = await fetch('https://zenquotes.io/api/random');
   const data = await response.json();
-  return data;
+  return `${data[0].q} — ${data[0].a}`;
 }
+
+module.exports = getCitation;
 
