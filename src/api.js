e7043b37caea4a0d334ad53fe4b7c216496cf361
rom2 guer <romainguerin033@gmail.com>
2025-10-16 19:04:16 +0200

    deployment avec render

--- src/api.js
+++ src/api.js
@@ -3,7 +3,6 @@ const fetch = require('node-fetch');
 async function getCitation() {
   const response = await fetch('https://zenquotes.io/api/random');
   const data = await response.json();
-  return `${data[0].q} — ${data[0].a}`;
+  return data;
 }
 
-module.exports = getCitation;
